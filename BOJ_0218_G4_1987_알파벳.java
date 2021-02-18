417d8eeffd3e633fc1aae6c49a1d9e7dae03e719
hoooons <78480984+hoooons@users.noreply.github.com>
2021-02-18 23:38:06 +0900

    BOJ_0218_G4_1987_알파벳
    Stack 사용시 시간초과 >>> visit boolean 배열 이용

--- BOJ_0218_G4_1987_알파벳.java
+++ BOJ_0218_G4_1987_알파벳.java
@@ -1,21 +1,17 @@
 package Algo;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 public class BOJ_0218_G4_1987_알파벳 {
 	
-	//dfs 로 찾으면서 뺵트래킹으로 boolean이용한 차단
-	//bfs 로 완전탐색 ?
-	
-	static Stack<Character> stack;
 	static int[] dx = {1,0,0,-1};
 	static int[] dy = {0,1,-1,0};
-	static int R,C,res=1,count=1;
+	static int R,C,res=0;
 	static char[][] arr;
+	static boolean[] visit;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		stack = new Stack<>();
 		R = Integer.parseInt(sc.next());
 		C = Integer.parseInt(sc.next());
 		
@@ -28,37 +24,25 @@ public class BOJ_0218_G4_1987_알파벳 {
 			}
 		}
 		
-		find(0,0);
+		visit = new boolean[26];
+		find(0,0,1);
 		System.out.println(res);
 	}
 	
-	private static void find(int r,int c) {
-		stack.add(arr[r][c]);
-		if(stack.size()!=count) {
-			while(count!=stack.size())
-				stack.pop();
-		}
-		outer : for(int k=0;k<4;k++) {
-			if(stack.size()!=count) {
-				while(count!=stack.size())
-					stack.pop();
-			}
+	private static void find(int r,int c,int cnt) {
+		visit[arr[r][c]-'A']=true;
+		res = res>cnt ? res : cnt;
+		
+		for(int k=0;k<4;k++) {
 			int nx = r + dx[k];
 			int ny = c + dy[k];
 			
 			if(nx<0 || nx>=R || ny<0 || ny>=C) continue;
 			
-			if(k==3 && stack.contains(arr[nx][ny])) {
-				count--;
-				return;
-			}
-			else if(k!=3 && stack.contains(arr[nx][ny]))
-				continue outer;
+			if(visit[arr[nx][ny]-'A']==true) continue;
 			
-			count++;
-			res = count>res ? count : res;
-			find(nx,ny);
+			find(nx,ny,cnt+1);
 		}
-		count--;
+		visit[arr[r][c]-'A']=false;
 	}
 }
